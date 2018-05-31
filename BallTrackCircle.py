84bce326cf7dfbe2add6d730baee3c53629f8207
faizansid77 <master.faizan83@gmail.com>
2018-06-01 04:18:42 +0530

    3 D Mapping Done

--- BallTrackCircle.py
+++ BallTrackCircle.py
@@ -1,28 +1,37 @@
 import cv2 as cv 
 import numpy as np 
 from imutils.video import VideoStream
-pinkl=(280,30,60)
-pinkh=(360,85,100)
+import imutils
+pinkl=(150,50,100)
+pinkh=(175,255,255)
 cap=VideoStream(src=1).start()
-img=cv.imread('img.png',1)
-img=cv.resize(img,(75,75))
 cv.namedWindow('Ball',cv.WINDOW_FULLSCREEN)
+cv.namedWindow('Mask',cv.WINDOW_FULLSCREEN)
 while True:
     frame=cap.read()
+    frame = imutils.resize(frame, width=600)
     if cv.waitKey(1) & 0xff==ord('q'):
         break
-    eframe=cv.bilateralFilter(frame,5,75,75)
+    eframe=cv.GaussianBlur(frame,(11,11),0)
     eframe=cv.cvtColor(eframe,cv.COLOR_BGR2HSV)
     mask=cv.inRange(eframe,pinkl,pinkh)
-    mask=cv.erode(mask,None,2)
-    mask=cv.dilate(mask,None,2)
-    _,cntr,_=cv.findContours(mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    mask=cv.erode(mask,None,iterations=2)
+    mask=cv.dilate(mask,None,iterations=2)
+    cntr=cv.findContours(mask.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)[-2]
     if len(cntr)>0:
-        c=max(cntr,cv.contourArea)
-        (x,y),radius=cv.minEnclosingCircle(c)
+        c=max(cntr,key=cv.contourArea)
+        for e in cntr:
+            cv.drawContours(frame,[e],-1,(255,0,0),1)
+        ((x,y),radius)=cv.minEnclosingCircle(c)
         center=(int(x),int(y))
         radius=int(radius)
-        if radius>10:
+        M=cv.moments(c)
+        centroid=(int(M["m10"]/M["m00"]),int(M["m01"]/M["m00"]))
+        if radius>5:
             cv.circle(frame,center,radius,(0,255,0),2)
+            cv.circle(frame,centroid,5,(0,0,255),-1)
+            dist=5.033512 + (611.8415-5.033512)/(1 + pow((radius/12.93463),1.196176))
+            print(int(x),"\t",int(y),"\t",int(dist))
     cv.imshow('Ball',frame)
+    cv.imshow('Mask',mask)
 cv.destroyAllWindows()
