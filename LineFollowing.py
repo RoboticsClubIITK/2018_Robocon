e837f0ea629f9c614201818c9cdedc53b72b775b
faizansid77 <master.faizan83@gmail.com>
2018-06-09 01:50:20 +0530

    line following updated

--- LineFollowing.py
+++ LineFollowing.py
@@ -9,7 +9,7 @@ while cv.waitKey(1) & 0xff!=ord('q'):
     flag, frame = capture.read() #read the video in frames
     gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)#convert each frame to grayscale.
     blur=cv.GaussianBlur(gray,(5,5),0)#blur the grayscale image
-    ret,th1 = cv.threshold(blur,35,255,cv.THRESH_BINARY+cv.THRESH_OTSU)#using threshold remave noise
+    ret,th1 = cv.threshold(blur,35,255,cv.THRESH_BINARY+cv.THRESH_OTSU)#using threshold remove noise
     ret1,th2 = cv.threshold(th1,127,255,cv.THRESH_BINARY_INV)# invert the pixels of the image frame
     th2 = cv.erode(th2, None, iterations=2)  
     th2 = cv.dilate(th2, None, iterations=2)
@@ -18,13 +18,13 @@ while cv.waitKey(1) & 0xff!=ord('q'):
     # im2=th2[:,int(h/2):int(3*h/4)]
     # im3=th2[:,int(h/4):int(h/2)]
     # im4=th2[:,0:int(h/4)]
-    im1=th2[int(3*h/4):int(h),:]
-    im2=th2[int(h/2):int(3*h/4),:]
-    im3=th2[int(h/4):int(h/2),:]
-    im4=th2[0:int(h/4),:]
-    im=[im1,im2,im3,im4]
+    # im1=th2[int(3*h/4):int(h),:]
+    # im2=th2[int(h/2):int(3*h/4),:]
+    # im3=th2[int(h/4):int(h/2),:]
+    # im4=th2[0:int(h/4),:]
+    # im=[im1,im2,im3,im4]
     for i in range(0,4):
-        _,contours, hierarchy = cv.findContours(im[i],cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE) #find the contours
+        _,contours, hierarchy = cv.findContours(th2[int((3-i)*h/4):int((4-i)*h/4),:],cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE) #find the contours
         if len(contours)>0:
             cnt=max(contours,key=cv.contourArea)
             area = cv.contourArea(cnt)# find the area of contour
@@ -33,6 +33,8 @@ while cv.waitKey(1) & 0xff!=ord('q'):
                 M = cv.moments(cnt)
                 cx = int(M['m10']/M['m00'])
                 cy = int((M['m01']/M['m00'])+(3-i)*h/4)
-                cv.drawContours(frame[int((3-i)*h/4):int((4-i)*h/4),:],cnt,-1,(0,255,0),3)
+                for e in contours:
+                    cv.drawContours(frame[int((3-i)*h/4):int((4-i)*h/4),:],[e],-1,(0,255,0),1)
                 cv.circle(frame,(cx,cy),5,(0,0,255),-1)
+                cv.circle(frame,(cx,cy),30,(255,0,0),2)
     cv.imshow('frame',frame) #show video 
